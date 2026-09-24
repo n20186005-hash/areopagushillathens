02e847e71a8625c9c18c791cdcc3e1b948a01914
n20186005-hash <n20186005@gmail.com>
2026-09-24 16:04:11 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
+import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   outputFileTracingRoot: process.cwd(),
   async redirects() {
     return [
       {
         source: '/:path*',
-        has: [{ type: 'host' as const, value: 'www.areopagushillathens.com' }],
+        has: [{ type: 'host', value: 'www.areopagushillathens.com' }],
         destination: 'https://areopagushillathens.com/:path*',
         permanent: true,
       },
